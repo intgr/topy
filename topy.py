92c6a3417de36090e868d94527912209f671f666
Marti Raudsepp <marti@juffo.org>
2014-05-22 23:02:29 +0300

    Refactor into functions, allow importing as a module

--- topy.py
+++ topy.py
@@ -23,23 +23,10 @@ import sys
 
 from bs4 import BeautifulSoup
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(message)s'
-)
-
 RETF_FILENAME = 'retf.txt'
 
-path = os.path.join(os.path.dirname(__file__), RETF_FILENAME)
-soup = BeautifulSoup(open(path))
-
-disabled = 0
-errors = 0
-parsed = 0
-
-regs = []
 # some rules are not working with regex or are not useful
-my_disable = {
+disabled = {
     "Etc.",
     "e.g.",
     "i.e.",
@@ -56,49 +43,81 @@ my_disable = {
     "Arabic",
 }
 
-for typo in soup.findAll('typo'):
-    if 'word' not in typo.attrs or typo.attrs['word'] in my_disable:
-        disabled += 1
-        continue
 
-    word = typo.attrs['word']
-    find = typo.attrs['find'].encode('utf8')
-    replace = typo.attrs['replace'].encode('utf8')
+def load_rules(filename):
+    """Load and parse rules from `filename`, returns list of 3-tuples [(name, regexp, replacement), ...]"""
+
+    soup = BeautifulSoup(open(filename))
+    regs = []
+
+    n_disabled = 0
+    n_errors = 0
+    n_loaded = 0
+
+    for typo in soup.findAll('typo'):
+        if 'word' not in typo.attrs or typo.attrs['word'] in disabled:
+            n_disabled += 1
+            continue
+
+        word = typo.attrs['word']
+        find = typo.attrs['find'].encode('utf8')
+        replace = typo.attrs['replace'].encode('utf8')
+
+        try:
+            r = regex.compile(find)
+            # Use \1 instead of $1 etc
+            replace = replace.replace(b'$', b'\\')
+
+            regs.append((word, r, replace))
+            n_loaded += 1
+        except regex.error as err:
+            logging.debug("cannot compile %s %r: %s" % (word, find, err))
+            n_errors += 1
+
+    logging.info("Loaded %d rules (except %d errors, %d disabled)" % (n_loaded, n_errors, n_disabled))
+
+    return regs
+
+
+def apply_to_file(regs, filename):
+    """Apply rules from `regs` to file `filename`, overwriting the file."""
 
     try:
-        r = regex.compile(find)
-        # Use \1 instead of $1 etc
-        replace = replace.replace(b'$', b'\\')
-
-        regs.append((word, r, replace))
-        parsed += 1
-    except regex.error as err:
-        logging.debug("cannot compile %s %r: %s" % (word, find, err))
-        errors += 1
-
-logging.info("Loaded %d rules (%d errors, %d disabled)" % (parsed, errors, disabled))
-
-
-for fn in sys.argv[1:]:
-    try:
-        with open(fn, 'rb') as f:
+        with open(filename, 'rb') as f:
             text = f.read()
     except (IOError, OSError) as err:
-        logging.error("Cannot open %r: %s" % (fn, err))
+        logging.error("Cannot open %r: %s" % (filename, err))
 
-    total = 0
+    replaced = 0
     for word, r, replace in regs:
         logging.debug(word, r, replace)
         try:
             newtext, count = r.subn(replace, text)
             if count > 0 and newtext != text:
-                total += count
-                logging.info("%s: replaced %s x %d" % (fn, word, count))
+                replaced += count
+                logging.info("%s: replaced %s x %d" % (filename, word, count))
             text = newtext
         except regex.error as err:
-            logging.error("%s: error replacing %s (%r=>%r): %s" % (fn, word, r, replace, err))
+            logging.error("%s: error replacing %s (%r=>%r): %s" % (filename, word, r, replace, err))
 
-    if total > 0:
-        logging.info("Writing %s" % fn)
-        with open(fn, 'wb') as f:
+    if replaced > 0:
+        logging.info("Writing %s" % filename)
+        with open(filename, 'wb') as f:
             f.write(text)
+
+
+def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(message)s'
+    )
+
+    path = os.path.join(os.path.dirname(__file__), RETF_FILENAME)
+    regs = load_rules(path)
+
+    for filename in sys.argv[1:]:
+        apply_to_file(regs, filename)
+
+
+if __name__ == '__main__':
+    main()
