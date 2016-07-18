7d3fefcb8d03ae985e6f44fe4664a42b71c7c5dc
Marti Raudsepp <marti@juffo.org>
2016-07-19 01:07:48 +0300

    Updated all rulesets from previous commit

--- update-all.sh
+++ update-all.sh
@@ -3,7 +3,6 @@
 
 wget "https://af.wikipedia.org/wiki/Wikipedia:AutoWikiBrowser/Typos?action=raw" -O topy/afwiki.txt
 wget "https://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7:%D8%A7%D9%84%D8%A3%D9%88%D8%AA%D9%88_%D9%88%D9%8A%D9%83%D9%8A_%D8%A8%D8%B1%D8%A7%D9%88%D8%B2%D8%B1/%D8%A7%D9%84%D9%85%D8%B3%D8%AA%D8%A8%D8%AF%D9%84%D8%A7%D8%AA?action=raw" -O topy/arwiki.txt
-wget "https://be-x-old.wikipedia.org/wiki/%D0%92%D1%96%D0%BA%D1%96%D0%BF%D1%8D%D0%B4%D1%8B%D1%8F:AutoWikiBrowser/Typos?action=raw" -O topy/be-x-oldwiki.txt
 wget "https://be.wikipedia.org/wiki/%D0%92%D1%96%D0%BA%D1%96%D0%BF%D0%B5%D0%B4%D1%8B%D1%8F:AutoWikiBrowser/Typos?action=raw" -O topy/bewiki.txt
 wget "https://ca.wikipedia.org/wiki/Viquip%C3%A8dia:AutoWikiBrowser/Typos?action=raw" -O topy/cawiki.txt
 wget "https://cs.wikipedia.org/wiki/Wikipedie:AutoWikiBrowser/Typos?action=raw" -O topy/cswiki.txt
@@ -15,6 +14,7 @@ wget "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:AutoWikiBrowser/Typos?action=
 wget "https://gl.wikipedia.org/wiki/Wikipedia:AutoWikiBrowser/Typos?action=raw" -O topy/glwiki.txt
 wget "https://hif.wikipedia.org/wiki/Wikipedia:AutoWikiBrowser/Typos?action=raw" -O topy/hifwiki.txt
 wget "https://hi.wikipedia.org/wiki/%E0%A4%B5%E0%A4%BF%E0%A4%95%E0%A4%BF%E0%A4%AA%E0%A5%80%E0%A4%A1%E0%A4%BF%E0%A4%AF%E0%A4%BE:AutoWikiBrowser/Typos?action=raw" -O topy/hiwiki.txt
-wget "https://hy.wikipedia.org/wiki/%D5%8E%D5%AB%D6%84%D5%AB%D5%BA%D5%A5%D5%A4%D5%AB%D5%A1:%D4%B1%D5%BE%D5%BF%D5%B8%D5%8E%D5%AB%D6%84%D5%AB%D4%B2%D6%80%D5%A1%D5%B8%D6%82%D5%A6%D5%A5%D6%80/%D5%8E%D6%80%D5%AB%D5%BA%D5%A1%D5%AF%D5%B6%D5%A5%D6%80?action=raw" -O topy/hywiki.txt
+wget "https://hy.wikipedia.org/wiki/%D5%8E%D5%AB%D6%84%D5%AB%D5%BA%D5%A5%D5%A4%D5%AB%D5%A1:AutoWikiBrowser/Typos?action=raw" -O topy/hywiki.txt
 wget "https://is.wikipedia.org/wiki/Wikipedia:AutoWikiBrowser/Typos?action=raw" -O topy/iswiki.txt
 
+grep -L '<Typo' topy/*.txt
