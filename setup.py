771dfc006568d24ec5709d0d28e16f7d579266f9
Marti Raudsepp <marti@juffo.org>
2021-01-30 16:34:39 +0200

    Test & document Python 3.9 support (#27)

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Documentation',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Text Processing :: Filters',
